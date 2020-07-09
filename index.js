7dfc5d64019dd739f6ded33d8a0ed48bde4d519b
Jorny <jorenfrans25@gmail.com>
2020-07-09 20:05:21 +0200

    Jornybot v1.0.1

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ const botConfig = require("./config.json");
 const client = new discord.Client();
 client.login(botConfig.token);
 
-client.on("ready", async () =>{
-
-console.log('De bot is actief');
-
-})
+client.on("ready", async () => {
+ 
+    console.log(`${client.user.username} is online.`);
+ 
+    client.user.setActivity("Testing", { type: "PLAYING" });
+ 
+});
+ 
+ 
+client.on("message", async message => {
+ 
+    if(message.author.bot) return;
+ 
+    if(message.channel.type === "dm") return;
+ 
+    var prefix = botConfig.prefix;
+ 
+    var messageArray = message.content.split(" ");
+ 
+    var command = messageArray[0];
+ 
+    if (command === `${prefix}hallo`) {
+ 
+        return message.channel.send("Hallo!!");
+   
+    }
+ 
+});
